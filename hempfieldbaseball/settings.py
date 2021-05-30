fd30aae051681a04e92395de0b7bb737d23e9139
Derek Manning <47646253+dmanning21h@users.noreply.github.com>
2021-05-30 16:19:29 -0400

    env for static root

--- hempfieldbaseball/settings.py
+++ hempfieldbaseball/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.environ.get('STATIC_ROOT')
 
 STATIC_URL = '/static/'
 
